63da0996b0cf37dce6c4f8f60d968c9d0c64a744
Jahfal102 <126544993+Jahfal102@users.noreply.github.com>
2024-06-26 00:31:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,30 +13,6 @@ import plotly.express as px
 
 import warnings
 warnings.filterwarnings('ignore')
-def set_background(image_file):
-    """
-    This function sets the background of a Streamlit app to an image specified by the given image file.
-
-    Parameters:
-        image_file (str): The path to the image file to be used as the background.
-
-    Returns:
-        None
-    """
-    with open(image_file, "rb") as f:
-        img_data = f.read()
-    b64_encoded = base64.b64encode(img_data).decode()
-    style = f"""
-        <style>
-        .stApp {{
-            background-image: url(data:image/png;base64,{b64_encoded});
-            background-size: cover;
-        }}
-        </style>
-    """
-    st.markdown(style, unsafe_allow_html=True)
-
-set_background('download(1).png')
 
 st.write("## Calories burned Prediction")
 st.write("In this WebApp you will be able to observe your predicted calories burned in your body. Only thing you have to do is pass your parameters such as `Age`, `Gender`, `BMI`, etc. into this WebApp and then you will be able to see the predicted value of kilocalories that burned in your body.")
